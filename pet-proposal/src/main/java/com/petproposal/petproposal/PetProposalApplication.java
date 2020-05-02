dc44bd2d1effda16906676e2871ae9798cdc6311
Nikola Saric <nikolasaric531@gmail.com>
2020-05-02 10:14:07 +0200

    initialization of Spring Boot project.

---
+++ pet-proposal/src/main/java/com/petproposal/petproposal/PetProposalApplication.java
@@ -0,0 +1,13 @@
+package com.petproposal.petproposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetProposalApplication.class, args);
+	}
+
+}
---
+++ pet-proposal/src/test/java/com/petproposal/petproposal/PetProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproposal.petproposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
